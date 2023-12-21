cc80fc880bf710cff784bf95af641e1862675532
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-21 20:51:46 +0530

    variable - passing arguments

---
+++ 4-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hello $Person2! Good morning"
+echo "$Person2: Hi $Person1 very good morining"
+echo "$Person1: How are you $Person2?"
+echo "$Person2: I am good $Person1. How are you?"
