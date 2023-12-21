4873e6913fb1052e446b62664cc4d981d8ba1abd
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-21 20:38:31 +0530

    second script

---
+++ 2.variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1: Hello $Person2! Good morning"
+echo "$Person2: Hi $Person1 very good morining"
+echo "$Person1: How are you $Person2?"
+echo "$Person2: I am good $Person1. How are you?"
