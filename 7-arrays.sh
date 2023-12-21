d8d1885c74f08716a8f73a774c241268fa7856a5
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-21 22:02:51 +0530

    arrays - we can store different values in same variables

---
+++ 7-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fruits=("apple" "banana" "guva")
+
+echo "First value : ${fruits[0]}"
+echo "second value : ${fruits[1]}"
+echo "Third value : ${fruits[2]}"
+echo "All values : ${fruits[@]}"
