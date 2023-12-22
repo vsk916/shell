ea0cfd7c6575c4b1eb95493b5f0b91ff60ae9fac
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-22 18:29:11 +0530

    log.sh; git push origin git add . ; git commit -m log.sh

--- 10-function.sh
+++ 10-function.sh
@@ -5,10 +5,10 @@ ID=$(id -u)
 validate (){ 
     if [ $1 -ne 0 ]
     then 
-        echo "$2 is failed"
+        echo "$2 is .......... failed"
         exit 1
     else
-        echo "$2 sucess"
+        echo "$2 .........sucess"
     fi
 }
 
---
+++ 11-logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+timestamp=$(date +%F-%H-%M-%S)
+
+logfile="/tmp/$0-$timestamp.log"
+
+validate () {
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is .........failure"
+    else
+        echo "$2 is .........success"
+    fi
+
+    }
+
+if [ $ID -ne 0 ]
+then 
+    echo "Error you are not in the root user"
+else
+    echo "You are in the root user"
+fi
+
+dnf install nginx -y
+
+validate $? "nginx is installing"
