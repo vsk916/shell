8e3cb1d9e7b08b78a76fedeae925c95efc37d25c
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-22 18:45:22 +0530

    log.sh; git push origin git add . ; git commit -m log.sh

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,6 +2,12 @@
 
 ID=$(id -u)
 
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+
 timestamp=$(date +%F-%H-%M-%S)
 
 logfile="/tmp/$0-$timestamp.log"
@@ -9,16 +15,16 @@ logfile="/tmp/$0-$timestamp.log"
 validate () {
     if [ $1 -ne 0 ]
     then 
-        echo "$2 is .........failure"
+        echo "$2 is $R.........failure$N"
     else
-        echo "$2 is .........success"
+        echo "$2 is $G.........success$N"
     fi
 
     }
 
 if [ $ID -ne 0 ]
 then 
-    echo "Error you are not in the root user"
+    echo "$R Error you are not in the root user$N"
 else
     echo "You are in the root user"
 fi
