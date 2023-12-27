9466c171a460b092490e54784a10685dc0edaa19
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-27 09:23:25 +0530

    delete old log files script

---
+++ 15-delete-old-logfile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source_file="/tmp/shellscript.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $source_file ]
+then
+echo -e "Source file : $source_file does not exit"
+fi
+
+file_to_delete=$(find . -type f -mtime +14 -name "*.log")
+
+while IFS=read -r line
+do 
+echo "Deleting file.....$line"
+#rm -rf $line
+
+done <<< $file_to_delete
