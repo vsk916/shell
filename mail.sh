707812e519ea34ed31bf27b3869d6f8d2387ccf0
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-28 15:30:13 +0530

    disk_usage with mail alert with Html format

--- mail.sh
+++ mail.sh
@@ -4,7 +4,7 @@ Alert_type=$2
 Body=$3
 To_Addre=$4
 Subject=$5
-Final_Body=$(sed -e 's/TO_TEAM/$TO_TEAM' -e '/Alert_type/$Alert_type' -e 's/Body/$Body' html-templete.html)
+Final_Body=$(sed -e 's/TO_TEAM/$TO_TEAM/g' -e '/Alert_type/$Alert_type/g' -e 's/Body/$Body/g' html-templete.html)
 
 
 echo "$Final_Body" | mail -s "$subject" "$To_Addre"
