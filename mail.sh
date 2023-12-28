c7a66716a8c6ed74a8e993fbd98a98f4c32478f8
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-28 15:52:57 +0530

    disk_usage with mail alert with Html format-subject variable name mistake

--- mail.sh
+++ mail.sh
@@ -4,7 +4,8 @@ Alert_type=$2
 Body=$3
 To_Addre=$4
 Subject=$5
+ESCAPE_BODY=$(printf '%s\n' "$Body" | sed -e 's/[]\/$*.^[]/\\&/g');
 Final_Body=$(sed -e 's/TO_TEAM/$TO_TEAM/g' -e 's/Alert_type/$Alert_type/g' -e 's/Body/$Body/g' html-templete.html)
 
 
-echo "$Final_Body" | mail -s "$Subject" "$To_Addre"
+echo "$Final_Body" | mail -s "$(echo -e $message\ncontent-type: text\html")" "$To_Addre"
