68a5cfa7738b104ca86eaa62d4ddc77a01eb710e
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-28 11:22:18 +0530

    disk_usage1.0

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -7,7 +7,7 @@ message=""
 while IFS= read -r line
 do 
   usage=$(echo $line |awk '{print $5F}'|cut -d % -f1)
-  partition=$(echo $line | awk '{print $1F})'
+  partition=$(echo $line | awk '{print $1F}')
   if [ $usage -ge $Disk_Threshold ]
   then
   message+="High disusage  on $partition: $usage"
