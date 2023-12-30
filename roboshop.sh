5ead9c17abf55af5cbb32e268eec0be0c2c3507f
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-30 21:20:15 +0530

    roboshop instance in shell script

--- roboshop.sh
+++ roboshop.sh
@@ -12,11 +12,14 @@ do
   else
       INSTANCE_TYPE=t2.micro
   fi
-   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-08efee20dd1ce7f9f --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateAddress' --output text)
+   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-08efee20dd1ce7f9f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateAddress' --output text)
 
   echo "$i: $IP_ADDRESS"
   
 done
+
+ #IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
 
 
 
