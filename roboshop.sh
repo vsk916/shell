7d86d88afa2d5997e880e59d9809b59d00fa957e
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-30 18:30:41 +0530

    roboshop instance in shell script

---
+++ roboshop.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+AMI_ID=ami-03265a0778a880afb
+SG_ID=sg-08efee20dd1ce7f9f
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do 
+  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+  then
+      INSTANCE_TYPE=t3.small 
+  else
+      INSTANCE_TYPE=t2.micro
+  fi
+  aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-08efee20dd1ce7f9f  
+  
+done
+
+
+
+
