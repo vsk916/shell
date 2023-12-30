9e7b9acda80da1c88d6793e78a3ee7440815fcf7
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-30 18:45:17 +0530

    roboshop instance in shell script

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do 
-  echo "Instance is: $1"
+  echo "Instance is: $i"
   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
   then
       INSTANCE_TYPE=t3.small 
