ea29b0cf3acd231b84ec4917c2327f84ced58a2d
vsk916 <pentelavamsisaikrishna@gmail.com>
2024-01-10 21:45:42 +0530

    changes in catalogu.yaml

--- roboshop.sh
+++ roboshop.sh
@@ -19,7 +19,7 @@ do
 done
 
  #IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "$i: $IP_ADDRESS"
+    
 
 
 
